5e881e8ea9f3d420a58fa7802c604649c9766eb2
Tiger Wang <ziwei.tiger@outlook.com>
2021-02-09 12:19:35 +0000

    Bump CMake minimum, pare down config.h

--- include/mbedtls/mbedtls_config.h
+++ include/mbedtls/mbedtls_config.h
@@ -1265,7 +1265,7 @@
  *
  * Enable the checkup functions (*_self_test).
  */
-#define MBEDTLS_SELF_TEST
+//#define MBEDTLS_SELF_TEST
 
 /**
  * \def MBEDTLS_SHA256_SMALLER
@@ -2440,7 +2440,7 @@
  *
  * This module provides networking routines.
  */
-#define MBEDTLS_NET_C
+//#define MBEDTLS_NET_C
 
 /**
  * \def MBEDTLS_OID_C
